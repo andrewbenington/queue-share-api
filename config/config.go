b7e8363408f17f012aac7bda734e247e9805e7c7
Andrew Benington <andrewbenington57@gmail.com>
2023-09-15 18:39:40 -0500

    use socket for db

--- config/config.go
+++ config/config.go
@@ -6,25 +6,33 @@ import (
 )
 
 type Config struct {
-	PostgresHost string
-	PostgresPort string
-	PostgresUser string
-	PostgresPass string
-	PostgresDB   string
+	PostgresHost   string
+	PostgresPort   string
+	PostgresUser   string
+	PostgresPass   string
+	PostgresDB     string
+	PostgresSocket string
+	IsGAE          bool
 }
 
 func GetConfig() Config {
 	return Config{
-		PostgresHost: os.Getenv("POSTGRES_HOST"),
-		PostgresPort: os.Getenv("POSTGRES_PORT"),
-		PostgresUser: os.Getenv("POSTGRES_USER"),
-		PostgresPass: os.Getenv("POSTGRES_PASS"),
-		PostgresDB:   os.Getenv("POSTGRES_DB"),
+		PostgresHost:   os.Getenv("POSTGRES_HOST"),
+		PostgresPort:   os.Getenv("POSTGRES_PORT"),
+		PostgresUser:   os.Getenv("POSTGRES_USER"),
+		PostgresPass:   os.Getenv("POSTGRES_PASS"),
+		PostgresDB:     os.Getenv("POSTGRES_DB"),
+		PostgresSocket: os.Getenv("POSTGRES_SOCKET"),
+		IsGAE:          os.Getenv("GAE_APPLICATION") != "",
 	}
 }
 
 func (c *Config) GetDBString() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
-		c.PostgresHost, c.PostgresPort, c.PostgresUser, c.PostgresPass, c.PostgresDB)
-
+	if c.IsGAE {
+		return fmt.Sprintf("%s:%s@unix(%s)/%s",
+			c.PostgresUser, c.PostgresPass, c.PostgresSocket, c.PostgresDB)
+	} else {
+		return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
+			c.PostgresHost, c.PostgresPort, c.PostgresUser, c.PostgresPass, c.PostgresDB)
+	}
 }
