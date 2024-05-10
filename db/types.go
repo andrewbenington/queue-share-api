99ce73286dae8796e8d6ceb53b43b7a95251b2e5
Andrew Benington <andrewbenington57@gmail.com>
2024-05-10 12:18:47 -0500

    sql scan interface for track ext ids

--- db/types.go
+++ db/types.go
@@ -1,5 +1,11 @@
 package db
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
 type TrackArtist struct {
 	ID   string `json:"id"`
 	URI  string `json:"uri"`
@@ -7,3 +13,31 @@ type TrackArtist struct {
 }
 
 type TrackExternalIDs map[string]string
+
+func (ids TrackExternalIDs) Value() (driver.Value, error) {
+	if len(ids) == 0 {
+		return "{}", nil
+	}
+	bytes, err := json.Marshal(ids)
+	if err != nil {
+		return nil, err
+	}
+	return string(bytes), nil
+}
+
+func (ids *TrackExternalIDs) Scan(src interface{}) (err error) {
+	var extIDs TrackExternalIDs
+	switch src := src.(type) {
+	case string:
+		err = json.Unmarshal([]byte(src), &extIDs)
+	case []byte:
+		err = json.Unmarshal(src, &extIDs)
+	default:
+		return errors.New("incompatible type for TrackExternalIDs")
+	}
+	if err != nil {
+		return
+	}
+	*ids = extIDs
+	return nil
+}
